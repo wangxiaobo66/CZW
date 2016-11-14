d3f13f1c809f917c6a20c0a9bf7f07410b8bb4bc
wangxiaobo <wangxiaobo@hbc315.com>
2016-11-15 00:00:42 +0800

    commit

---
+++ controller/router-controller.js
@@ -0,0 +1,14 @@
+/**
+ * Created by wangxiaobo on 16/8/25.
+ */
+var views = require('co-views');
+var render = views(__dirname,{map: {html: 'swig'}});
+
+module.exports = {
+    index: function *(next){
+        this.body = yield render('../template/index.html');
+    },
+    login: function *(next){
+        this.body = yield render('../template/login.html');
+    }
+};
